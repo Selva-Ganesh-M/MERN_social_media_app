5351ad5e5880824762aec9191febe41997c936e9
Selva Ganesh M <selvadev2k@gmail.com>
2022-12-11 12:16:36 +0530

    install pacakges and setup configuration

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import multer from "multer";
+import path from "path";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+
+// configuration
+// below 2 lines are only for "type": "module" confiuration
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// middlewares
+dotenv.config();
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+server.use(morgan("common"));
+server.use(bodyParser.json({ limit: "30mb", extended: true }));
+server.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+server.use(cors());
+server.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// file storage
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, res, cb) {
+    cb(null, file.originalname);
+  },
+});
